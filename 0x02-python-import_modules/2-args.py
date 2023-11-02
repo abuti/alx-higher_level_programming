22e928519360a240ce924d86e159ae23efaf6a8c
abuti <besuneg2006@gmail.com>
2023-11-02 11:56:45 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
     else:
-        print("{} argument:".format(len(sys.argv) - 1))
+        if len(sys.argv) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
