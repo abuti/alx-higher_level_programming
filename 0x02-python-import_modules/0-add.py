12113c6418aba813bb22d2e52435118f8c742537
abuti <besuneg2006@gmail.com>
2023-11-02 10:29:47 -0700

    0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 import add_0
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+
+
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+
+
+if __name__ == "__main__":
+    main()
