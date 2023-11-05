4f1281c3f9dfa3651cf72430568f56fe96c1aad7
abuti <besuneg2006@gmail.com>
2023-11-04 23:49:36 -0700

    0 and 3

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
 
 
 if __name__ == "__main__":
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.sort(reverse=True)
     for i in my_list:
-        print("{d}".format(i))
+        print("{:d}".format(i))
