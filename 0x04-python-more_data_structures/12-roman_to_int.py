1d28ff5cfca0fa751786b6e75ed45bf487b7e829
abuti <besuneg2006@gmail.com>
2023-11-08 15:56:24 -0800

    12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+             'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+    n = 0
+    for i in roman_string:
+        n += roman[i]
+    return n
