f245b67a142ad7ec75892cbb5f510c511e1db709
abuti <besuneg2006@gmail.com>
2023-11-08 12:49:56 -0800

    1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in my_list:
+        if i == search:
+            i = replace
+        new_list.append(i)
+
+    return new_list
