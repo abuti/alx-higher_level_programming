ac70e54847b1686d3642398a41a78c9e586bbcfe
abuti <besuneg2006@gmail.com>
2023-11-08 16:32:19 -0800

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,6 +5,10 @@ def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
              'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
     n = 0
-    for i in roman_string:
-        n += roman[i]
+    for i in range(len(roman_string)):
+        if i != len(roman_string) - 1:
+            if roman[roman_string[i]] < roman[roman_string[i + 1]]:
+                n = n - roman[roman_string[i]]
+                continue
+        n += roman[roman_string[i]]
     return n
