dca49431b940534bcb543b12c3d647a0e46feafa
abuti <besuneg2006@gmail.com>
2023-11-23 23:05:12 -0800

    1

--- 0x05-python-exceptions/1-main.py
+++ 0x05-python-exceptions/1-main.py
@@ -15,3 +15,8 @@ value = "School"
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
+
+value = [89]
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("Wrong format")
