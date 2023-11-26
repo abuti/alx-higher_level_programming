c9fc2c8977e2752f66d71b71f265b1cb538f3d0d
abuti <besuneg2006@gmail.com>
2023-11-25 23:57:53 -0800

    3

--- 0x05-python-exceptions/3-main.py
+++ 0x05-python-exceptions/3-main.py
@@ -6,7 +6,7 @@ b = 2
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
 
-a = 12
-b = 0
+a = 10
+b = 2
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
