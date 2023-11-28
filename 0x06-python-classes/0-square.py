06fae613f5308d199747bb33514b59457956f299
abuti <besuneg2006@gmail.com>
2023-11-27 23:36:22 -0800

    creating empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
+# This module or class does nothing
+# it is just used to show how an empty class
+# is created.
+
+
 class Square:
-    pass #An empty block
+    pass  # An empty block
