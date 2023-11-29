4cb776c7f16c6d660e7502612040c4be13a99f60
abuti <besuneg2006@gmail.com>
2023-11-28 22:44:21 -0800

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" Square with size
+
+This module depicts how to initialize a class object
+and how to use private instances."""
+
+
 class Square:
     """This is a class that defines a square with size."""
 
